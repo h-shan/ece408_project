82b40f8f39d72c23839ed7808f59169fb9d1c1d6
h-shan <howardshan11@gmail.com>
2019-03-31 20:45:07 -0500

    Write kernel code for shared memory convolution, does not work

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -3,11 +3,15 @@
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
 
 #include <mxnet/base.h>
+#include <stdio.h>
 
 namespace mxnet
 {
 namespace op
 {
+
+
+#define TILE_SIZE 16
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
@@ -28,8 +32,49 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
     #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
     #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+    
+    int n, m, h0, w0, h_base, w_base, h, w;
+    int X_tile_width = TILE_SIZE + K-1;
+    extern __shared__ float shmem[];
+    float* X_shared = &shmem[0];
+    float* W_shared = &shmem[X_tile_width * X_tile_width];
+    const int W_grid = W_out/TILE_SIZE; // number of horizontal tiles per output map
+    n = blockIdx.x;
+    m = blockIdx.y;
+    h0 = threadIdx.x;
+    w0 = threadIdx.y;
+    h_base = (blockIdx.z / W_grid) * TILE_SIZE; // vertical base out data index for the block
+    w_base = (blockIdx.z % W_grid) * TILE_SIZE; // horizontal base out data index for the block
+    h = h_base + h0;
+    w = w_base + w0;
+    
+    float acc = 0.;
+    int c, p, q;
+    for (c = 0; c < C; c++) { // sum over all input channels
+      if (( h0 < K) && ( w0 < K)) {
+        W_shared[h0 * K + w0]= k4d(m, c, h0, w0);
+      }
+      __syncthreads();
+   // load tile from X[n, c,…] into shared memory
 
-        
+      for (int i = h; i < h_base + X_tile_width; i += TILE_SIZE) {
+        for (int j = w; j < w_base + X_tile_width; j += TILE_SIZE) {
+          X_shared[(i - h_base) * X_tile_width + j - w_base] = x4d(n, c, h, w);
+        }
+      }
+      __syncthreads();
+      for (p = 0; p < K; p++) {
+        for (q = 0; q < K; q++) {
+          if (h0 + p < X_tile_width && w0 + q < X_tile_width) {
+            acc = acc + X_shared[(h0 + p) * X_tile_width + w0 + q] * W_shared[p * K + q];
+          }
+        }
+      }
+      __syncthreads();
+    }
+    if (h < H && w < W) {
+      y4d(n, m, h, w) = acc; 
+    }
 
     #undef y4d
     #undef x4d
@@ -53,27 +98,28 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int B = x.shape_[0];  // Batch Size
     const int M = y.shape_[1];  // Output Channels (Feature Maps)
     const int C = x.shape_[1];  // Input Channels (Feature Maps) 
-    const int H = x.shape[2];  // Output height (y)
-    const int W = x.shape[3];  // Output width (x)
-    const int K = w.shape[3];  // Filter Width/Height
-
-    #define TILE_SIZE 16
+    const int H = x.shape_[2];  // Output height (y)
+    const int W = x.shape_[3];  // Output width (x)
+    const int K = w.shape_[3];  // Filter Width/Height
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-    Z = (W_out / TILE_SIZE) * (H_out / TILE_SIZE);
-
+    printf("B: %d\nM: %d\nC: %d\nH: %d\nW: %d\nK: %d\n", B, M, C, H, W, K);
+    
+    const int Z = ceil((W_out + 0.0) / TILE_SIZE) * ceil((H_out + 0.0)/ TILE_SIZE);
+    // const int Z = (W_out / TILE_SIZE) * (H_out / TILE_SIZE);
+    
     // Set the kernel dimensions
     dim3 gridDim(B, M, Z);
     dim3 blockDim(TILE_SIZE, TILE_SIZE, 1);
 
+    size_t shmem_size = sizeof(float) * ( (TILE_SIZE + K-1) * (TILE_SIZE + K-1) + K*K );
     // Call the kernel
-    forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_, x.dptr_, w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<gridDim, blockDim, shmem_size>>>(y.dptr_, x.dptr_, w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
-
 }
 
 /* 
