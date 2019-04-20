aefd0353ce491bc3ca30111dd3c06f98afde39e7
h-shan <howardshan11@gmail.com>
2019-04-20 17:05:04 -0500

    Implement matrix multiplication (is slow)

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -4,20 +4,191 @@
 
 #include <mxnet/base.h>
 #include <stdio.h>
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+
 namespace mxnet
 {
 namespace op
 {
 
 
-#define TILE_SIZE 16
+#define TILE_SIZE 8
 #define KERNEL_SIZE 5
-__constant__ float cst_ptr [KERNEL_SIZE * KERNEL_SIZE * 500]; // 50 max number of channels
+#define MAX_NUM_THREADS 1024
+__constant__ float cst_ptr[KERNEL_SIZE * KERNEL_SIZE * 500]; // 50 max number of channels
 
+
+__global__ void unroll_Kernel2(int C, int H, int W, int K, float *X, float *X_unroll) {
+  // unrolls K * K for H * W * c
+  int c = blockIdx.y;
+  int t = blockIdx.x * blockDim.x + threadIdx.x;
+  const int H_out = H + K - 1;
+  const int W_out = W + K - 1;
+  const int total_elements = H_out * W_out;
+
+  #define x3d(i2, i1, i0) X[(i2) * (H * W) + (i1) * (W) + (i0)]
+  #define x_unroll2d(i1, i0) X_unroll[total_elements * (i1) + (i0)]
+  if (t < total_elements) {
+    int h = t / W_out;
+    int w = t % W_out;
+    int w_base = c * K * K;
+    int h_unroll = h * W_out + w;
+    for (int i = 0; i < K; i++) {
+      for (int j = 0; j < K; j++) {
+        int w_unroll = w_base + i * K + j;
+        x_unroll2d(w_unroll, h_unroll) = x3d(c, h + i, w + j);
+      }
+    }
+  }
+
+  #undef x3d
+  #undef x_unroll2d
+}
+
+__global__ void unroll_Kernel(int C, int H, int W, int K, float *X, float *X_unroll)
+{
+  int c, s, h_out, w_out, h_unroll, w_unroll, w_base, p, q, m;
+  int t = blockIdx.x * blockDim.x + threadIdx.x;
+  int H_out = H - K + 1;
+  int W_out = W - K + 1;
+  int total_elements = H_out * W_out;
+
+  #define x3d(i2, i1, i0) X[(i2) * (H * W) + (i1) * (W) + (i0)]
+  #define x_unroll2d(i1, i0) X_unroll[total_elements * (i1) + (i0)]
+
+  c = threadIdx.y;
+  if (t < total_elements) {
+    h_out = t / W_out;
+    w_out = t % W_out;
+    h_unroll = h_out * W_out + w_out;
+    w_base = c * K * K;
+    for(p = 0; p < K; p++) {
+      for(q = 0; q < K; q++) {
+        w_unroll = w_base + p * K + q;
+        x_unroll2d(w_unroll, h_unroll) = x3d(c, h_out + p, w_out + q);
+      }
+    }
+  }
+
+  #undef x3d
+  #undef x_unroll2d
+}
+
+
+void unroll_gpu(int C, int H, int W, int K, float *X, float *X_unroll)
+{
+  int H_out = H - K + 1;
+  int W_out = W - K + 1;
+  int num_threads = H_out * W_out;
+  int num_blocks = ceil((num_threads * C + 0.0) / (MAX_NUM_THREADS / C * C));
+  int num_blocks2 = ceil((H_out * W_out * C + 0.0) / MAX_NUM_THREADS);
+  dim3 gridDim(num_blocks, C, 1);
+  dim3 blockDim(MAX_NUM_THREADS / C, C, 1);
+
+  unroll_Kernel<<<num_blocks, blockDim>>>(C, H, W, K, X, X_unroll);
+  // unroll_Kernel2<<<gridDim, MAX_NUM_THREADS>>>(C, H, W, K, X, X_unroll);
+}
+
+__global__ void unroll(int C, int H, int W, int K, float* X, float* X_unroll)
+{
+  #define x3d(i2, i1, i0) X[(i2) * (H * W) + (i1) * (W) + (i0)]
+  #define x_unroll2d(i1, i0) X_unroll[total_elements * (i1) + (i0)]
+  int c, h, w, p, q, w_base, w_unroll, h_unroll;
+  int H_out = H - K + 1;
+  int W_out = W - K + 1;
+  int total_elements = W_out * H_out;
+  for(c = 0; c < C; c++) {
+    w_base = c * (K*K);
+    for(p = 0; p < K; p++) {
+      for(q = 0; q < K; q++) {
+        for(h = 0; h < H_out; h++) {
+          for(w = 0; w < W_out; w ++) {
+            w_unroll = w_base + p * K + q;
+            h_unroll = h * W_out + w;
+            x_unroll2d(w_unroll, h_unroll) = x3d(c, h + p, w + q); 
+          }
+        }
+      }
+    }
+  }
+}
+
+template <>
+void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w)
+{
+  const int N = x.shape_[0];  // Batch Size
+  const int M = y.shape_[1];  // Output Channels (Feature Maps)
+  const int C = x.shape_[1];  // Input Channels (Feature Maps) 
+  const int H = x.shape_[2];  // Output height (y)
+  const int W = x.shape_[3];  // Output width (x)
+  const int K = w.shape_[3];  // Filter Width/Height
+
+  printf("w: %d %d %d %d\n", w.shape_[0], w.shape_[1], w.shape_[2], w.shape_[3]);
+  printf("x: %d %d %d %d\n", x.shape_[0], x.shape_[1], x.shape_[2], x.shape_[3]);
+  printf("y: %d %d %d %d\n", y.shape_[0], y.shape_[1], y.shape_[2], y.shape_[3]);
+  int W_out = W - K + 1;
+  int H_out = H - K + 1;
+  int ckk = C * K * K;
+  int total_elements = H_out * W_out;
+  
+  cublasHandle_t handle;
+  cublasCreate(&handle);
+
+  float *X_unrolled;
+  cudaMalloc((void **) &X_unrolled, ckk * total_elements * sizeof(float));
+
+  int m = M, k = ckk;
+  const float alf = 1;
+  const float bet = 0;
+  const float *alpha = &alf;
+  const float *beta = &bet;
+
+  // float mat_a[12] = {
+  //   1, 2, 3,
+  //   4, 5, 6,
+  //   7, 8, 9,
+  //   10, 11, 12
+  // };
+  // float mat_b[6] = {
+  //   1, 2,
+  //   3, 4,
+  //   5, 6
+  // };
+
+  // float *mat_c = (float *) malloc(8 * sizeof(float));
+  // float *d_a, *d_b, *d_c;
+  // cudaMalloc(&d_a, 12 * sizeof(float));
+  // cudaMalloc(&d_b, 6 * sizeof(float));
+  // cudaMalloc(&d_c, 8 * sizeof(float));
+  // cudaMemcpy(d_a, mat_a, 12 * sizeof(float), cudaMemcpyHostToDevice);
+  // cudaMemcpy(d_b, mat_b, 6 * sizeof(float), cudaMemcpyHostToDevice);
+  // // (4x3) * (3x2) = (4x2)
+  // // (M x ckk) * (ckk x total_elements) = (M x total_elements)
+  // cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, 2, 4, 3, alpha, d_b, 2, d_a, 3, beta, d_c, 2);
+  // cudaMemcpy(mat_c, d_c, 8 * sizeof(float), cudaMemcpyDeviceToHost);
+  // for (int i = 0; i < 4; i++) {
+  //   for (int j = 0; j < 2; j++) {
+  //     printf("%.2f ", mat_c[i * 2 + j]);
+  //   }
+  //   printf("\n");
+  // }
+
+  for (int n=0; n < N; n++) {
+    unroll_gpu(C, H, W, K, x.dptr_ + (H * W * C * n), X_unrolled);
+    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
+      total_elements, M, ckk, alpha,
+      X_unrolled, total_elements,
+      w.dptr_, ckk,
+      beta,
+      y.dptr_ + (n * M * total_elements), total_elements);
+  }
+  cublasDestroy(handle);
+  MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
+}
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
-
     /*
     Modify this function to implement the forward pass described in Chapter 16.
     We have added an additional dimension to the tensors to support an entire mini-batch
@@ -113,7 +284,7 @@ __global__ void forward_kernel_original(float *y, const float *x, const float *k
    Any code you write should be executed by this function.
    For ECE408, we only expect the float version of the operator to be called, so here we specialize with only floats.
 */
-template <>
+/*template <>
 void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w)
 {
     // Use mxnet's CHECK_EQ to do assertions.
@@ -139,11 +310,11 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     cudaMemcpyToSymbol(cst_ptr, w.dptr_, KERNEL_SIZE * KERNEL_SIZE * M * C * sizeof(float), 0, cudaMemcpyDeviceToDevice);
     size_t shmem_size = sizeof(float) * ( (TILE_SIZE + K-1) * (TILE_SIZE + K-1) * 2);
     // Call the kernel
-    forward_kernel<<<gridDim, blockDim, shmem_size>>>(y.dptr_, x.dptr_, w.dptr_, B,M,C,H,W,K);
+    forward_kernel_original<<<gridDim, blockDim, shmem_size>>>(y.dptr_, x.dptr_, w.dptr_, B,M,C,H,W,K);
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
-}
+}*/
 
 /* 
     This tells mxnet how to do an op when it's not a float.
