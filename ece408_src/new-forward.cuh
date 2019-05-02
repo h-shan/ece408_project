d35799441eb9f7b15b8158e712e923e82d615f11
h-shan <howardshan11@gmail.com>
2019-05-02 16:03:52 -0500

    Use shared memory convolution for 1 input kernel layer

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -14,6 +14,7 @@ namespace op
 #define TILE_SIZE 8
 #define KERNEL_SIZE 5
 #define MAX_NUM_THREADS 1024
+__constant__ float cst_ptr [KERNEL_SIZE * KERNEL_SIZE * 500]; // 50 max number of channels
 
 __global__ void matrixMultiplyShared(float *A, float *B, float *C,
   int numARows, int numAColumns,
@@ -64,6 +65,67 @@ __global__ void matrixMultiplyShared(float *A, float *B, float *C,
       C[row * numCColumns + col] = p;
     }
   }
+}
+
+__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
+{
+    /*
+    Modify this function to implement the forward pass described in Chapter 16.
+    We have added an additional dimension to the tensors to support an entire mini-batch
+    The goal here is to be correct AND fast.
+    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
+    */
+
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+
+    // An example use of these macros:
+    // float a = y4d(0,0,0,0)
+    // y4d(0,0,0,0) = a
+    #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+    #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+    #define k4d(i3, i2, i1, i0) cst_ptr[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+    
+    int n, m, h0, w0, h_base, w_base, h, w;
+    int X_tile_width = TILE_SIZE + K-1;
+    extern __shared__ float shmem[];
+    float* X_shared1 = &shmem[0];
+    float* X_shared2 = &shmem[X_tile_width * X_tile_width];
+    const int W_grid = ceil((W_out + 0.0) / TILE_SIZE); // number of horizontal tiles per output map
+    n = blockIdx.x;
+    m = blockIdx.y;
+    h0 = threadIdx.x;
+    w0 = threadIdx.y;
+    h_base = (blockIdx.z / W_grid) * TILE_SIZE; // vertical base out data index for the block
+    w_base = (blockIdx.z % W_grid) * TILE_SIZE; // horizontal base out data index for the block
+    h = h_base + h0;
+    w = w_base + w0;
+    
+    float acc = 0.;
+    int c, p, q;
+    for (c = 0; c < C; c++) { // sum over all input channels
+      float *X_shared = c % 2 ? X_shared1 : X_shared2;
+      for (int i = h; i < h_base + X_tile_width; i += TILE_SIZE) {
+        for (int j = w; j < w_base + X_tile_width; j += TILE_SIZE) {
+          if (i < H && j < W) {
+            X_shared[(i - h_base) * X_tile_width + j - w_base] = x4d(n, c, i, j);
+          }
+        }
+      }
+      __syncthreads();
+      for (p = 0; p < K; p++) {
+        for (q = 0; q < K; q++) {
+          acc += X_shared[(h0 + p) * X_tile_width + w0 + q] * k4d(m, c, p, q);
+        }
+      }
+    }
+    if (h < H_out && w < W_out) {
+      y4d(n, m, h, w) = acc; 
+    }
+
+    #undef y4d
+    #undef x4d
+    #undef k4d
 }
 
 __global__ void unroll_Kernel(int C, int H, int W, int K, float *X, float *X_unroll)
@@ -122,24 +184,37 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
   int W_out = W - K + 1;
   int H_out = H - K + 1;
-  int ckk = C * K * K;
-  int total_elements = H_out * W_out;
-  float *X_unrolled;
-  cudaMalloc((void **) &X_unrolled, N * ckk * total_elements * sizeof(float));
+  if (C == 1) {
+    const int Z = ceil((W_out + 0.0) / TILE_SIZE) * ceil((H_out + 0.0)/ TILE_SIZE);
+    
+    // Set the kernel dimensions
+    dim3 gridDim(N, M, Z);
+    dim3 blockDim(TILE_SIZE, TILE_SIZE, 1);
 
-  int numARows = M;
-  int numAColumns = ckk;
-  int numBRows = ckk;
-  int numBColumns = total_elements;
-  int numCRows = M;
-  int numCColumns = total_elements;
-
-  unroll_gpu(C, H, W, K, N, x.dptr_, X_unrolled);
-  dim3 dimGrid(ceil(numCColumns/(TILE_SIZE + 0.0)), ceil(numCRows/(TILE_SIZE + 0.0)), N);
-  dim3 dimBlock(TILE_SIZE, TILE_SIZE, 1);
-  matrixMultiplyShared<<<dimGrid, dimBlock>>>(w.dptr_, X_unrolled, y.dptr_, numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns, ckk, total_elements, M);
-  MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
-  cudaFree(X_unrolled);
+    cudaMemcpyToSymbol(cst_ptr, w.dptr_, KERNEL_SIZE * KERNEL_SIZE * M * C * sizeof(float), 0, cudaMemcpyDeviceToDevice);
+    size_t shmem_size = sizeof(float) * ( (TILE_SIZE + K-1) * (TILE_SIZE + K-1) * 2);
+    // Call the kernel
+    forward_kernel<<<gridDim, blockDim, shmem_size>>>(y.dptr_, x.dptr_, w.dptr_, N,M,C,H,W,K);
+  } else {
+    int ckk = C * K * K;
+    int total_elements = H_out * W_out;
+    float *X_unrolled;
+    cudaMalloc((void **) &X_unrolled, N * ckk * total_elements * sizeof(float));
+  
+    int numARows = M;
+    int numAColumns = ckk;
+    int numBRows = ckk;
+    int numBColumns = total_elements;
+    int numCRows = M;
+    int numCColumns = total_elements;
+  
+    unroll_gpu(C, H, W, K, N, x.dptr_, X_unrolled);
+    dim3 dimGrid(ceil(numCColumns/(TILE_SIZE + 0.0)), ceil(numCRows/(TILE_SIZE + 0.0)), N);
+    dim3 dimBlock(TILE_SIZE, TILE_SIZE, 1);
+    matrixMultiplyShared<<<dimGrid, dimBlock>>>(w.dptr_, X_unrolled, y.dptr_, numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns, ckk, total_elements, M);
+    MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
+    cudaFree(X_unrolled);
+  }
 }
 
 template <typename gpu, typename DType>
